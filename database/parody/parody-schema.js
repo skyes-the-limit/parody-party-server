f1c2688a104a7dc35b99c563d59b4c65bd7494c0
skyes-the-limit <riversong477@gmail.com>
2022-04-24 16:20:46 -0400

    updates for viewing parodies

--- database/parody/parody-schema.js
+++ database/parody/parody-schema.js
@@ -4,6 +4,7 @@ const parodySchema = mongoose.Schema(
   {
     originalGeniusID: { type: Number, required: true },
     author: { type: String, required: true },
+    title: { type: String, required: true },
     lyrics: { type: String, required: true },
     likes: { type: Number, default: 0 },
     comments: { type: Array },
