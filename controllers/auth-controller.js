29ae9221d1d9d713bf3df9125261ade9a7aa7e72
skyes-the-limit <riversong477@gmail.com>
2022-04-27 12:49:54 -0400

    Create auth-controller

---
+++ controllers/auth-controller.js
@@ -0,0 +1,49 @@
+import usersDao from '../database/users/users-dao.js'
+
+const AUTH_API_BASE = '/api/auth'
+
+const authController = (app) => {
+  app.post(`${AUTH_API_BASE}/signup`, signup)
+  app.post(`${AUTH_API_BASE}/profile`, profile)
+  app.post(`${AUTH_API_BASE}/signin`, login)
+  app.post(`${AUTH_API_BASE}/logout`, logout)
+}
+
+const signup = async (req, res) => {
+  const credentials = req.body
+  const existingUser = await usersDao.findUserByEmail(credentials.email)
+  if (existingUser) {
+    return res.sendStatus(403)
+  } else {
+    const newUser = await usersDao.createUser(credentials)
+    req.session.profile = newUser
+    res.json(newUser)
+  }
+}
+
+const login = async (req, res) => {
+  const credentials = req.body
+  const profile = await usersDao.findUserByCredentials(credentials.email, credentials.password)
+  if (profile) {
+    req.session.profile = profile
+    res.json(profile)
+    return
+  }
+  res.sendStatus(403)
+}
+
+const profile = (req, res) => {
+  const profile = req.session.profile
+  if (profile) {
+    res.json(profile)
+  } else {
+    res.sendStatus(503)
+  }
+}
+
+const logout = (req, res) => {
+  req.session.destroy()
+  res.sendStatus(200)
+}
+
+export default authController
