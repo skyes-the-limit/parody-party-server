150575532d3ee8e1eb92183de7964fc2ffdd9c9e
skyes-the-limit <riversong477@gmail.com>
2022-04-28 19:25:10 -0400

    Fix login

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -5,7 +5,7 @@ const AUTH_API_BASE = '/api/auth'
 const authController = (app) => {
   app.post(`${AUTH_API_BASE}/signup`, signup)
   app.post(`${AUTH_API_BASE}/profile`, profile)
-  app.post(`${AUTH_API_BASE}/signin`, login)
+  app.post(`${AUTH_API_BASE}/login`, login)
   app.post(`${AUTH_API_BASE}/logout`, logout)
 }
 
@@ -26,9 +26,10 @@ const login = async (req, res) => {
   const profile = await usersDao.findUserByCredentials(credentials.username, credentials.password)
   if (profile) {
     req.session.profile = profile
-    return
+    res.json(profile)
+  } else {
+    res.sendStatus(403)
   }
-  res.sendStatus(403)
 }
 
 const profile = (req, res) => {
