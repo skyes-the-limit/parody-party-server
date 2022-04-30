e7abe8b8b57f35c7074542a603c603d8127c797c
skyes-the-limit <riversong477@gmail.com>
2022-04-29 22:04:03 -0400

    Allow CORS from remote host

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ mongoose.connect(CONNECTION_STRING)
 app.use(cors(
   {
     credentials: true,
-    origin: 'http://localhost:3000'
+    // origin: 'http://localhost:3000'
+    origin: 'https://parody-party.netlify.app/'
   }
 ))
 app.use(session({
